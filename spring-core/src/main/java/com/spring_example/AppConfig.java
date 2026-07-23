fa4613d8556bb8515aad59c5fb924417a331f543
Chertes Victor <victor.chertes.vc@gmail.com>
2026-07-23 14:08:48 +0300

    XML Configuration Spring Core

---
+++ spring-core/src/main/java/com/spring_example/AppConfig.java
@@ -0,0 +1,16 @@
+package com.spring_example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean(name = "HelloWorld")
+    public HelloWorld helloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+
+        helloWorld.setMessage("Hello World!");
+
+        return helloWorld;
+    }
+}
