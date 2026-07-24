57e848bebc92a1036bfe7e320b1150f1c6a1d478
Chertes Victor <victor.chertes.vc@gmail.com>
2026-07-24 10:17:45 +0300

    Resolve final changes for PR

--- spring-core/src/main/java/com/spring_example/config/AppConfig.java
+++ spring-core/src/main/java/com/spring_example/config/AppConfig.java
@@ -1,9 +1,16 @@
 package com.spring_example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import com.spring_example.model.HelloWorld;
 
 @Configuration
 @ComponentScan(basePackages = "com.spring_example")
 public class AppConfig {
+    @Bean
+    public HelloWorld helloWorld() {
+        return new HelloWorld();
+    }
+
 }
--- spring-core/src/main/java/com/spring_example/model/HelloWorld.java
+++ spring-core/src/main/java/com/spring_example/model/HelloWorld.java
@@ -2,8 +2,6 @@ package com.spring_example.model;
 
 public class HelloWorld {      
   
-   private String message;
-  
    public void getMessage(){
      System.out.println("Hello World ");
    }
