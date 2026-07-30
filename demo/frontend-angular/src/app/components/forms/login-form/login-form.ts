e244775b32cf489388c8b6aad31181c6e258317e
Chertes Victor <victor.chertes.vc@gmail.com>
2026-07-30 17:07:37 +0300

    Finish chapter 209-typed-reactive-forms

---
+++ demo/frontend-angular/src/app/components/forms/login-form/login-form.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  NonNullableFormBuilder,
+  Validators,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+import { AuthService } from '../../../services/auth/auth-service';
+
+type LoginForm = {
+  email: FormControl<string>;
+  password: FormControl<string>;
+};
+
+@Component({
+  selector: 'app-login-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login-form.html',
+  styleUrl: './login-form.css',
+})
+export class LoginComponent {
+  private readonly _formBuilder = inject(NonNullableFormBuilder);
+
+  private readonly _authService = inject(AuthService);
+
+  protected readonly loginFormGroup = this._formBuilder.group<LoginForm>({
+    email: this._formBuilder.control('', [Validators.required, Validators.email]),
+    
+    password: this._formBuilder.control('', [Validators.required, Validators.minLength(6)])
+  });
+
+  onFormSubmit(): void {
+    if (this.loginFormGroup.valid) {
+      console.log('Form Submitted successfully!');
+      console.log('Raw Values:', this.loginFormGroup.getRawValue());
+
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
--- demo/frontend-angular/src/app/components/login/login.ts
+++ demo/frontend-angular/src/app/components/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../forms/login-form/login-form';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
